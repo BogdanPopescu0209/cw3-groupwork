c07ada947cea041b746ad6658697ce35b4fbb167
BogdanPopescu0209 <59745574+BogdanPopescu0209@users.noreply.github.com>
2022-03-02 15:33:35 +0000

    function to add files to cache

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,18 @@ let cacheFiles = [
     'images/512x512.png',
     'images/192x192.png'
 ]
+
+self.addEventListener('install', (event) => {
+
+    console.log('[service worker] install');
+
+    event.waitUntil(
+
+        caches.open(cacheName).then((cache) => {
+
+            console.log('[service worker] caching files');
+
+            return cache.addAll(cacheFiles);
+        })
+    )
+});
